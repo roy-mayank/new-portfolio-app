d83c485e8f441049564cc69b4c7557be041ddf4f
Mayank Roy <roy050703@gmail.com>
2024-08-18 11:57:10 +0530

    fixed unnecessary import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar.component";
 import { Link } from "react-router-dom";
